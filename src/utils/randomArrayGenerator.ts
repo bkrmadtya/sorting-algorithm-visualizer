574f2152f549974f1bf8351a7a47a098a3e69e42
bkrmadtya <karki.bikram007@gmail.com>
2021-01-23 22:15:17 +0200

    feat: added randomArrayGeneartor method with its helper methods

---
+++ src/utils/randomArrayGenerator.ts
@@ -0,0 +1,20 @@
+import Bar from 'src/utils/Bar'
+import { BarStatus } from 'src/utils/enum'
+
+const generateRandomSetOfNumbers = (sizeOfArray: number): Set<number> => {
+	const set = new Set<number>()
+
+	while (set.size < sizeOfArray) {
+		const randomNumber = Math.floor(Math.random() * (sizeOfArray * 5)) + 10
+		set.add(randomNumber)
+	}
+
+	return set
+}
+
+const randomArrayGenerator = (sizeOfArray: 10): Array<Bar> => {
+	const randomArray = Array.from(generateRandomSetOfNumbers(sizeOfArray))
+	return randomArray.map(height => new Bar(height, BarStatus.UNSORTED))
+}
+
+export default randomArrayGenerator
