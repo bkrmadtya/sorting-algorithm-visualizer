17e0d563f8df5e5940a39993e8e6b4aa8c42ef06
bkrmadtya <karki.bikram007@gmail.com>
2021-01-23 22:56:26 +0200

    feat: added InitialState and its interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 // style
 import 'src/app.scss'
 
 // components
-import Bar from 'src/components/Bar'
+import BarComponent from 'src/components/Bar'
 
 // utils
 import randomArrayGenerator from 'src/utils/randomArrayGenerator'
+import Bar from 'src/utils/Bar'
+
+interface IInitialState {
+	steps: Bar[][]
+	currentStep: number
+	timeouts: []
+	delay: number
+}
+
+const initailArray = randomArrayGenerator(10)
+
+const initialState: IInitialState = {
+	steps: [[...initailArray]],
+	currentStep: 0,
+	timeouts: [],
+	delay: 200
+}
 
 const App: React.FC = () => {
+	const [state, setState] = useState(initialState)
+
 	return (
 		<div className='app'>
 			<h1>Sorting algorithm visualizer</h1>
 
 			<div className='bar__container'>
 				{randomArrayGenerator(10).map(a => (
-					<Bar key={a.value} value={a.value} status={a.status} />
+					<BarComponent key={a.value} value={a.value} status={a.status} />
 				))}
 			</div>
 		</div>
