c2457b13f6789ef85e6b14f706ae3b155a58ce1d
bkrmadtya <karki.bikram007@gmail.com>
2021-01-23 21:55:09 +0200

    feat: added BarColors object with values of BarStatus enum as keys

---
+++ src/utils/BarColors.ts
@@ -0,0 +1,11 @@
+import { BarStatus } from 'src/utils/enum'
+
+const BarColors = {
+	[BarStatus.ACTIVE]: 'orange',
+	[BarStatus.LESSER]: 'red',
+	[BarStatus.GREATER]: 'green',
+	[BarStatus.SORTED]: 'green',
+	[BarStatus.UNSORTED]: 'grey'
+}
+
+export default BarColors
