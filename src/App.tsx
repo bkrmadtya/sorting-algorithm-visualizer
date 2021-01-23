87e90ef0a02789e97cf84b1bc68e5ec830b0bd35
bkrmadtya <karki.bikram007@gmail.com>
2021-01-23 23:26:01 +0200

    feat: added sort and cancel method along with useEffect to set the steps for the first time

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 // style
 import 'src/app.scss'
 
@@ -8,11 +8,12 @@ import BarComponent from 'src/components/Bar'
 // utils
 import randomArrayGenerator from 'src/utils/randomArrayGenerator'
 import Bar from 'src/utils/Bar'
+import bubbleSort from './algorithms/bubbleSort'
 
 interface IInitialState {
 	steps: Bar[][]
 	currentStep: number
-	timeouts: []
+	timeouts: NodeJS.Timeout[]
 	delay: number
 }
 
@@ -22,21 +23,63 @@ const initialState: IInitialState = {
 	steps: [[...initailArray]],
 	currentStep: 0,
 	timeouts: [],
-	delay: 200
+	delay: 0
 }
 
 const App: React.FC = () => {
 	const [state, setState] = useState(initialState)
+
+	useEffect(() => {
+		const sortedSteps = bubbleSort(state.steps[0])
+		setState({ ...state, steps: [...sortedSteps] })
+	}, [])
+
+	const cancel = () => {
+		state.timeouts.forEach(t => clearTimeout(t))
+		setState({ ...state, timeouts: [] })
+	}
+
+	const sort = () => {
+		let i = 0
+		const timeouts = []
+
+		cancel()
+
+		if (state.currentStep >= state.steps.length - 1) {
+			return
+		}
+
+		while (i < state.steps.length - 1 - state.currentStep) {
+			const timeout = setTimeout(() => {
+				setState(prevState => {
+					const currentStep = prevState.currentStep
+
+					return {
+						...prevState,
+						currentStep: currentStep + 1
+					}
+				})
+			}, state.delay * i)
+
+			timeouts.push(timeout)
+			i++
+		}
+
+		setState({ ...state, timeouts: timeouts })
+	}
 
 	return (
 		<div className='app'>
 			<h1>Sorting algorithm visualizer</h1>
 
 			<div className='bar__container'>
-				{randomArrayGenerator(10).map(a => (
+				{state.steps[state.currentStep].map(a => (
 					<BarComponent key={a.value} value={a.value} status={a.status} />
 				))}
 			</div>
+
+			<button onClick={sort}>Sort</button>
+			<button onClick={cancel}>Cancel</button>
 		</div>
 	)
 }
