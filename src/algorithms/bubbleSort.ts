516f3f7f34ea7fd994bd344d2eb56ca0d309dfd2
bkrmadtya <karki.bikram007@gmail.com>
2021-01-27 20:24:06 +0200

    feat: changed bubbleSort function to a class which implements IAlgorithm interface

--- src/algorithms/bubbleSort.ts
+++ src/algorithms/bubbleSort.ts
@@ -1,26 +1,28 @@
 import Bar from 'src/utils/Bar'
+import { IAlgorithm } from 'src/utils/interface'
 import changeStatusOfElement from 'src/utils/changeStatusOfElement'
-import { BarStatus } from 'src/utils/enum'
 import swapElements from 'src/utils/swapElements'
+import { BarStatus } from 'src/utils/enum'
 
 const { ACTIVE, LESSER, GREATER, SORTED, UNSORTED } = BarStatus
 
-const bubbleSort = (arr: Bar[]): Bar[][] => {
-	const steps: Bar[][] = []
-	steps.push([...arr], [...arr])
+export default class BubbleSort implements IAlgorithm {
+	private steps: Bar[][] = []
 
-	const sort = () => {
+	public sort(arr: Bar[]): Bar[][] {
+		this.steps.push([...arr], [...arr])
+
 		let length = arr.length
 
 		while (length) {
-			const result = [...steps[steps.length - 1]]
+			const result = [...this.steps[this.steps.length - 1]]
 			let first = { ...result[0] }
 
 			for (let j = 1; j < length; j++) {
 				const second = result[j]
 
-				changeStatusOfElement(steps, first, ACTIVE)
-				changeStatusOfElement(steps, second, ACTIVE)
+				changeStatusOfElement(this.steps, first, ACTIVE)
+				changeStatusOfElement(this.steps, second, ACTIVE)
 
 				if (first.value > second.value) {
 					swapElements(result, first, second)
@@ -28,19 +30,15 @@ const bubbleSort = (arr: Bar[]): Bar[][] => {
 					first = result[j]
 				}
 
-				steps.push([...result])
-				changeStatusOfElement(steps, first, UNSORTED)
-				changeStatusOfElement(steps, second, UNSORTED)
+				this.steps.push([...result])
+				changeStatusOfElement(this.steps, first, UNSORTED)
+				changeStatusOfElement(this.steps, second, UNSORTED)
 			}
 
-			changeStatusOfElement(steps, first, SORTED)
+			changeStatusOfElement(this.steps, first, SORTED)
 			length--
 		}
+
+		return this.steps
 	}
-
-	sort()
-
-	return steps
 }
-
-export default bubbleSort
