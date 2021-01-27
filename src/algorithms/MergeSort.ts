faf70cd78d13a3f522911931e46eaf462dde217d
bkrmadtya <karki.bikram007@gmail.com>
2021-01-27 22:30:56 +0200

    feat: added MergeSort class with basic structure

---
+++ src/algorithms/MergeSort.ts
@@ -0,0 +1,14 @@
+import Bar from 'src/utils/Bar'
+import { IAlgorithm } from 'src/utils/interface'
+
+export default class MergeSort implements IAlgorithm {
+	private steps: Bar[][] = []
+
+	public sort(arr: Bar[]): Bar[][] {
+		return this.steps
+	}
+
+	private merge(): void {
+		return
+	}
+}
