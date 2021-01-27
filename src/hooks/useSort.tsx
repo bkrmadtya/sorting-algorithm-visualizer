f60f6b26907937cdbb746821daa6fb60ac5f4202
bkrmadtya <karki.bikram007@gmail.com>
2021-01-27 22:12:37 +0200

    feat: added useSort custom hook

---
+++ src/hooks/useSort.tsx
@@ -0,0 +1,126 @@
+import { useEffect, useState } from 'react'
+import Bar from 'src/utils/Bar'
+import randomArrayGenerator from 'src/utils/randomArrayGenerator'
+import { ISortingAlgorithm } from 'src/utils/interface'
+
+interface IState {
+	steps: Bar[][]
+	currentStep: number
+	timeouts: NodeJS.Timeout[]
+	delay: number
+}
+
+const initailArray = randomArrayGenerator(10)
+
+const initialState: IState = {
+	steps: [[...initailArray]],
+	currentStep: 0,
+	timeouts: [],
+	delay: 50
+}
+
+const useSort = (
+	sortingAlgo: ISortingAlgorithm
+): {
+	state: IState
+	sort: () => void
+	reset: () => void
+	cancel: () => void
+	previousStep: () => void
+	nextStep: () => void
+} => {
+	const [state, setState] = useState(initialState)
+	const [sortingAlgorithm] = useState(new sortingAlgo())
+
+	useEffect(() => {
+		console.log('INSIDE USE EFFECT :: ', initialState)
+		const sortingSteps = sortingAlgorithm.sort(state.steps[0])
+		setState({
+			...state,
+			steps: [...sortingSteps]
+		})
+	}, [])
+
+	const cancel = (): void => {
+		state.timeouts.forEach(t => clearTimeout(t))
+		setState({ ...state, timeouts: [] })
+	}
+
+	const sort = (): void => {
+		let i = 0
+		const timeouts = []
+
+		cancel()
+
+		if (state.currentStep >= state.steps.length - 1) {
+			return
+		}
+
+		while (i < state.steps.length - 1 - state.currentStep) {
+			const timeout = setTimeout(() => {
+				setState(prevState => {
+					const currentStep = prevState.currentStep
+
+					return {
+						...prevState,
+						currentStep: currentStep + 1
+					}
+				})
+			}, state.delay * i)
+
+			timeouts.push(timeout)
+			i++
+		}
+
+		setState({ ...state, timeouts: timeouts })
+	}
+
+	const reset = (): void => {
+		cancel()
+		const newArray = randomArrayGenerator(10)
+		const newSteps = sortingAlgorithm.sort(newArray)
+		setState({
+			steps: [...newSteps],
+			currentStep: 0,
+			timeouts: [],
+			delay: 50
+		})
+	}
+
+	const previousStep = (): void => {
+		cancel()
+
+		if (state.currentStep <= 0) {
+			return
+		}
+
+		setState(prevState => {
+			const currentStep = prevState.currentStep
+			return {
+				...prevState,
+				currentStep: currentStep - 1
+			}
+		})
+	}
+
+	const nextStep = (): void => {
+		cancel()
+
+		if (state.currentStep >= state.steps.length - 1) {
+			return
+		}
+
+		setState(prevState => {
+			const currentStep = prevState.currentStep
+
+			return {
+				...prevState,
+				currentStep: currentStep + 1
+			}
+		})
+	}
+
+	return { state, sort, cancel, reset, previousStep, nextStep }
+}
+
+export default useSort
