063c7fed1b74a02b451890bb0d6266492d35b769
bkrmadtya <karki.bikram007@gmail.com>
2021-01-30 14:22:15 +0200

    feat: added mergeSort, removeFirstElementAndReturn, getLeastInde and changeElementPosition

--- src/algorithms/MergeSort.ts
+++ src/algorithms/MergeSort.ts
@@ -1,14 +1,147 @@
 import Bar from 'src/utils/Bar'
+import changeStatusOfElement from 'src/utils/changeStatusOfElement'
+import { BarStatus } from 'src/utils/enum'
 import { IAlgorithm } from 'src/utils/interface'
+
+const { ACTIVE, UNSORTED, SORTED } = BarStatus
 
 export default class MergeSort implements IAlgorithm {
 	private steps: Bar[][] = []
 
 	public sort(arr: Bar[]): Bar[][] {
+		this.steps = []
+		this.steps.push([...arr], [...arr])
+		this.mergeSort(arr)
 		return this.steps
 	}
 
-	private merge(): void {
-		return
+	private mergeSort(arr: Bar[]): Bar[] {
+		if (arr.length < 2) {
+			return arr
+		}
+
+		const pivot: number = Math.floor(arr.length / 2)
+
+		let leftArr = arr.slice(0, pivot)
+		let rightArr = arr.slice(pivot, arr.length)
+
+		leftArr = this.mergeSort(leftArr)
+		rightArr = this.mergeSort(rightArr)
+
+		const mergedArray = this.merge(leftArr, rightArr)
+
+		return mergedArray
+	}
+
+	private merge(leftArr: Bar[], rightArr: Bar[]): Bar[] {
+		const sortedArr: Bar[] = []
+		const leastIndex = this.getLeastIndex(this.steps, leftArr[0], rightArr[0])
+
+		while (leftArr.length && rightArr.length) {
+			const isLeftEleGreater = leftArr[0].value > rightArr[0].value
+			const currentIndex = sortedArr.length
+
+			changeStatusOfElement(this.steps, leftArr[0], ACTIVE)
+			changeStatusOfElement(this.steps, rightArr[0], ACTIVE)
+
+			const lesserEle = isLeftEleGreater
+				? this.removeFirstElementAndReturn(rightArr)
+				: this.removeFirstElementAndReturn(leftArr)
+
+			sortedArr.push(lesserEle)
+
+			// move lesser element to the least position
+			const modifiedStep = this.changeElementPosition(
+				this.steps,
+				lesserEle,
+				leastIndex,
+				currentIndex
+			)
+
+			this.steps.push(modifiedStep)
+
+			if (
+				leftArr.length + rightArr.length + sortedArr.length ===
+				this.steps[0].length
+			) {
+				changeStatusOfElement(this.steps, lesserEle, SORTED)
+			} else {
+				changeStatusOfElement(this.steps, lesserEle, UNSORTED)
+				const greaterEle = isLeftEleGreater ? leftArr[0] : rightArr[0]
+				changeStatusOfElement(this.steps, greaterEle, UNSORTED)
+			}
+		}
+
+		while (leftArr.length) {
+			sortedArr.push(this.removeFirstElementAndReturn(leftArr))
+
+			if (
+				leftArr.length + rightArr.length + sortedArr.length ===
+				this.steps[0].length
+			) {
+				changeStatusOfElement(
+					this.steps,
+					sortedArr[sortedArr.length - 1],
+					SORTED
+				)
+			}
+		}
+
+		while (rightArr.length) {
+			sortedArr.push(this.removeFirstElementAndReturn(rightArr))
+
+			if (
+				leftArr.length + rightArr.length + sortedArr.length ===
+				this.steps[0].length
+			) {
+				changeStatusOfElement(
+					this.steps,
+					sortedArr[sortedArr.length - 1],
+					SORTED
+				)
+			}
+		}
+
+		return sortedArr
+	}
+
+	// Copy of Array.shift(): T | undefined but it return T
+	private removeFirstElementAndReturn(arr: Bar[]): Bar {
+		const firstEle = new Bar(arr[0].value, arr[0].status)
+		arr = [...arr.splice(0, 1)]
+		return firstEle
+	}
+
+	// get least index among two element from the last step of Bar[]
+	private getLeastIndex(steps: Bar[][], eleA: Bar, eleB: Bar): number {
+		const indexOfA = steps[steps.length - 1].findIndex(
+			ele => ele.value === eleA.value
+		)
+		const indexOfB = steps[steps.length - 1].findIndex(
+			ele => ele.value === eleB.value
+		)
+
+		return Math.min(indexOfA, indexOfB)
+	}
+
+	// change position of the element/bar inside an Bar[]
+	private changeElementPosition = (
+		steps: Bar[][],
+		eleToMove: Bar,
+		currentIndex: number,
+		leastIndex: number
+	): Bar[] => {
+		const lastStep = [...steps[steps.length - 1]]
+		const indexOfLesserEle = lastStep.findIndex(
+			ele => ele.value === eleToMove.value
+		)
+
+		lastStep.splice(
+			leastIndex + currentIndex,
+			0,
+			lastStep.splice(indexOfLesserEle, 1)[0]
+		)
+
+		return lastStep
 	}
 }
