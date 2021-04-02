38d1faf6bc473595cdeb1d649695966652a1a598
bikram <bikram@vertics.co>
2021-04-02 23:19:06 +0300

    fix: fixed a typo

--- src/utils/swapElements.ts
+++ src/utils/swapElements.ts
@@ -11,11 +11,11 @@ const swapElements = (
 	const indexOfFirstEle = getIndexOfElement(arr, firstEle)
 	const indexOfSecondEle = getIndexOfElement(arr, secondEle)
 
-	const tempFrist: Bar = new Bar(firstEle.value, firstEle.status)
+	const tempFirst: Bar = new Bar(firstEle.value, firstEle.status)
 	const tempSecond: Bar = new Bar(secondEle.value, secondEle.status)
 
 	arr[indexOfFirstEle] = tempSecond
-	arr[indexOfSecondEle] = tempFrist
+	arr[indexOfSecondEle] = tempFirst
 
 	return [...arr]
 }
