a2bbe73116a0c3f57aae8258faa7a3815e9b7230
bikram <bikram@vertics.co>
2021-04-05 17:12:54 +0300

    feat: created separate component for Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,16 @@
 import React from 'react'
+
 // style
 import 'src/app.scss'
 
-// hooks
-import useSort from './hooks/useSort'
-
 // components
-import BarComponent from 'src/components/Bar'
-
-// utils
-import BubbleSort from 'src/algorithms/BubbleSort'
-import MergeSort from 'src/algorithms/MergeSort'
-import randomArrayGenerator from './utils/randomArrayGenerator'
-import QuickSort from './algorithms/QuickSort'
-
-// const arr = randomArrayGenerator(10)
-// const sorted = new QuickSort().sort(arr)
-// console.log(sorted)
+import DashBoard from 'src/components/Dashboard'
 
 const App: React.FC = () => {
-	// const { state, sort, reset, cancel, previousStep, nextStep } = useSort(
-	// 	MergeSort
-	// )
-	const { state, sort, reset, cancel, previousStep, nextStep } = useSort(
-		QuickSort
-	)
-
 	return (
 		<div className='app'>
 			<h1>Sorting algorithms visualizer</h1>
-
-			<div className='bar__container'>
-				{state.steps[state.currentStep].map(a => (
-					<BarComponent key={a.value} value={a.value} status={a.status} />
-				))}
-			</div>
-
-			<button onClick={sort}>Sort</button>
-			<button onClick={cancel}>Cancel</button>
-			<button onClick={reset}>Reset</button>
-			<button onClick={previousStep}>Previous Step</button>
-			<button onClick={nextStep}>Next Step</button>
+			<DashBoard />
 		</div>
 	)
 }
