97415ed4e5b0a0a7809b4f73eebf74444076e53a
bikram <bikram@vertics.co>
2021-04-18 17:14:53 +0300

    feat: added style to the navbar header

--- src/components/Common/NavBar/index.tsx
+++ src/components/Common/NavBar/index.tsx
@@ -4,7 +4,7 @@ import { Header } from 'src/components/Common'
 const NavBar: React.FC = () => {
 	return (
 		<div className='navbar'>
-			<Header>Sorting Algorithm Visualizer</Header>
+			<Header className='navbar__header'>SORTING ALGORITHM VISUALIZER</Header>
 		</div>
 	)
 }
