bc25faa8c562f808ab180727cd6fda7f93b1b03e
bkrmadtya <karki.bikram007@gmail.com>
2021-04-18 19:31:13 +0300

    feat: added routing with react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 // style
 import 'src/app.scss'
@@ -10,8 +11,23 @@ import { Header, NavBar } from 'src/components/Common'
 const App: React.FC = () => {
 	return (
 		<div className='app'>
-			<NavBar />
-			<DashBoard />
+			<Router>
+				<NavBar>
+					<Link to='/algorithms'>ALGORITHMS</Link>
+					<Link to='/about'>ABOUT</Link>
+				</NavBar>
+				<Switch>
+					<Route path='/algorithms'>
+						<h1>Algorithms</h1>
+					</Route>
+					<Route path='/about'>
+						<h1>About</h1>
+					</Route>
+					<Route path='/'>
+						<DashBoard />
+					</Route>
+				</Switch>
+			</Router>
 		</div>
 	)
 }
--- src/components/Common/NavBar/index.tsx
+++ src/components/Common/NavBar/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
+// components
 import { Header } from 'src/components/Common'
 
-const NavBar: React.FC = () => {
+interface IProps {
+	children?: React.ReactNode
+}
+
+const NavBar: React.FC<IProps> = ({ children }) => {
 	return (
 		<div className='navbar'>
-			<Header className='navbar__header'>SORTING ALGORITHM VISUALIZER</Header>
+			<Header className='navbar__header'>
+				<Link to='/'>SORTING ALGORITHM VISUALIZER</Link>
+			</Header>
+			<div className='navbar__content'>{children}</div>
 		</div>
 	)
 }
