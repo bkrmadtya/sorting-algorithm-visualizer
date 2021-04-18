45e0edbefeb42b553829fb122b0b00c1364279c2
bkrmadtya <karki.bikram007@gmail.com>
2021-04-18 15:18:20 +0300

    feat: added Header component

---
+++ src/components/Common/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface IProps {
+	children: any
+	className?: string
+}
+
+const Header: React.FC<IProps> = ({ children, className }: IProps) => {
+	return <h1 className={className}>{children}</h1>
+}
+
+export default Header
