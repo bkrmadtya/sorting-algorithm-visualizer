e9ebf6e1b528eef679569e80fda4c1732f436454
bikram <bikram@vertics.co>
2021-04-22 23:41:30 +0300

    feat: added options for size and speed

---
+++ src/components/Dashboard/OptionContainer/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Header } from 'src/components/Common'
+import Dropdown from 'src/components/Common/Dropdown'
+
+const sizeOptions = [
+	{
+		key: '10',
+		value: '10'
+	},
+	{
+		key: '25',
+		value: '25'
+	},
+	{
+		key: '50',
+		value: '50'
+	}
+]
+
+const speedOptions = [
+	{
+		key: '1x',
+		value: '1x'
+	},
+	{
+		key: '2x',
+		value: '2x'
+	},
+	{
+		key: '4x',
+		value: '4x'
+	}
+]
+
+const OptionContainer: React.FC = () => {
+	return (
+		<div className='optionContainer'>
+			<Header className='panelHeader'>
+				<span>Size: </span>
+				<Dropdown options={sizeOptions} />
+			</Header>
+			<Header className='panelHeader'>
+				<span>Speed: </span>
+				<Dropdown options={speedOptions} />
+			</Header>
+		</div>
+	)
+}
+
+export default OptionContainer
