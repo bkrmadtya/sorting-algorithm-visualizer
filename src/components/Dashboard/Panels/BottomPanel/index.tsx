32b7bd47a052aa77f348097649ad041ab8119e7f
bikram <karki.bikram007@gmail.com>
2021-04-25 12:30:31 +0300

    feat: added table in the the bottom pannel

--- src/components/Dashboard/Panels/BottomPanel/index.tsx
+++ src/components/Dashboard/Panels/BottomPanel/index.tsx
@@ -1,13 +1,58 @@
 import React from 'react'
 import { Header } from 'src/components/Common'
 
+interface IRowData {
+	key: string
+	value: string | number | Array<number>
+}
+interface ITableData {
+	tableHead: string
+	tableRows: IRowData[]
+}
+
+const tableData: ITableData = {
+	tableHead: 'DETAILS',
+	tableRows: [
+		{
+			key: 'Size',
+			value: `25`
+		},
+		{
+			key: 'Initial Array',
+			value: `[88, 95, 117, 133, 48, 17, 123, 54, 37, 92, 74, 59, 124, 84, 113, 20, 94, 66, 73, 22, 39, 91, 97, 64, 13]`
+		},
+		{
+			key: 'Current Array',
+			value: `[13, 17, 20, 22, 37, 39, 48, 54, 59, 64, 66, 73, 74, 84, 88, 91, 92, 94, 95, 97, 113, 117, 123, 124, 133]`
+		},
+		{
+			key: 'Active Elements',
+			value: `10, 45`
+		},
+		{
+			key: 'Status',
+			value: `Unsorted`
+		}
+	]
+}
+
 const BottomPanel: React.FC = () => {
 	return (
 		<div className='bottomPanel'>
-			<Header className='panelHeader'>Logs:</Header>
-			<Header className='panelHeader'>Array:</Header>
-			<Header className='panelHeader'>Active elements:</Header>
-			<Header className='panelHeader'>Status:</Header>
+			<table className='detailTable'>
+				<tbody>
+					{tableData.tableRows.map(row => {
+						return (
+							<tr className='rowData' key={row.key}>
+								<td className='rowData__key'>
+									<strong>{row.key}</strong>
+								</td>
+								<td className='rowData__value'>{row.value}</td>
+							</tr>
+						)
+					})}
+				</tbody>
+			</table>
 		</div>
 	)
 }
