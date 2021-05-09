230e10473566f49cffa429432ba48a4e84dfe112
bikram <karki.bikram007@gmail.com>
2021-05-09 21:24:50 +0300

    feat: added onChange optional props method on Dropdown component

--- src/shared/Dropdown/index.tsx
+++ src/shared/Dropdown/index.tsx
@@ -5,11 +5,12 @@ type IProps = {
 		key: string
 		value: string
 	}[]
+	onChange?: React.ReactEventHandler
 }
 
-const Dropdown: React.FC<IProps> = ({ options }) => {
+const Dropdown: React.FC<IProps> = ({ options, onChange }) => {
 	return (
-		<select className='dropdown'>
+		<select className='dropdown' onChange={onChange}>
 			{options.map(({ key, value }) => (
 				<option key={key} value={value}>
 					{key}
