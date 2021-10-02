6dac967d6a7e3dee878eb1b5373bb47aad648a82
bkrmadtya <karki.bikram007@gmail.com>
2021-10-02 14:43:49 +0300

    fix: changed indentation to spaces

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,27 @@ import { NavBar } from './shared'
 import { Home, Algorithms, About } from './pages'
 
 const App: React.FC = () => {
-	return (
-		<div className='app'>
-			<Router>
-				<NavBar>
-					<Link to='/algorithms'>ALGORITHMS</Link>
-					<Link to='/about'>ABOUT</Link>
-				</NavBar>
-				<Switch>
-					<Route path='/algorithms'>
-						<Algorithms />
-					</Route>
-					<Route path='/about'>
-						<About />
-					</Route>
-					<Route path='/'>
-						<Home />
-					</Route>
-				</Switch>
-			</Router>
-		</div>
-	)
+  return (
+    <div className='app'>
+      <Router>
+        <NavBar>
+          <Link to='/algorithms'>ALGORITHMS</Link>
+          <Link to='/about'>ABOUT</Link>
+        </NavBar>
+        <Switch>
+          <Route path='/algorithms'>
+            <Algorithms />
+          </Route>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 export default App
