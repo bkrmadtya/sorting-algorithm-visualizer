a13c010b4f8a738716484ca66ea94e49167cb020
bkrmadtya <karki.bikram007@gmail.com>
2021-10-02 20:28:09 +0300

    fix: corrected import

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 export * from './Bar'
 export { default as changeStatusOfElement } from './changeStatusOfElement'
 export * from './interface'
-export * from './randomArrayGenerator'
+export { default as randomArrayGenerator } from './randomArrayGenerator'
 export { default as Steps } from './Steps'
 export { default as swapElements } from './swapElements'
