d116ad5d4cee1192c42268b4a6de36a0485c65cd
bkrmadtya <karki.bikram007@gmail.com>
2021-10-02 14:27:06 +0300

    fix: changed indentation to spaces

--- src/pages/Algorithms/index.tsx
+++ src/pages/Algorithms/index.tsx
@@ -3,12 +3,12 @@ import React, { useState } from 'react'
 // components
 import ContentContainer from './components/ContentContainer'
 import {
-	BubbleSort,
-	BigONotation,
-	Definition,
-	Measurement,
-	MergeSort,
-	QuickSort
+  BubbleSort,
+  BigONotation,
+  Definition,
+  Measurement,
+  MergeSort,
+  QuickSort
 } from './components/contents'
 import SideMenus from './components/SideMenu'
 
@@ -16,33 +16,33 @@ import SideMenus from './components/SideMenu'
 import { ITableOfContent } from '../../utils'
 
 const tableOfContent: ITableOfContent[] = [
-	{ id: 'definition', title: 'Definition', component: Definition },
-	{
-		id: 'measurement',
-		title: 'Measurement and Efficiency',
-		component: Measurement
-	},
-	{ id: 'big-o-notation', title: 'Big(O) notation', component: BigONotation },
-	{ id: 'bubble-sort', title: 'Bubble Sort', component: BubbleSort },
-	{ id: 'merge-sort', title: 'Merge Sort', component: MergeSort },
-	{ id: 'quick-sort', title: 'Quick Sort', component: QuickSort }
+  { id: 'definition', title: 'Definition', component: Definition },
+  {
+    id: 'measurement',
+    title: 'Measurement and Efficiency',
+    component: Measurement
+  },
+  { id: 'big-o-notation', title: 'Big(O) notation', component: BigONotation },
+  { id: 'bubble-sort', title: 'Bubble Sort', component: BubbleSort },
+  { id: 'merge-sort', title: 'Merge Sort', component: MergeSort },
+  { id: 'quick-sort', title: 'Quick Sort', component: QuickSort }
 ]
 
 const Algorithms: React.FC = () => {
-	const [selected, setSelected] = useState('algorithms')
+  const [selected, setSelected] = useState('algorithms')
 
-	const handleTabSelection = (title: string) => setSelected(title)
+  const handleTabSelection = (title: string) => setSelected(title)
 
-	return (
-		<div className='algorithmsPage'>
-			<ContentContainer tableOfContent={tableOfContent} />
-			<SideMenus
-				onClick={handleTabSelection}
-				selected={selected}
-				tableOfContent={tableOfContent}
-			/>
-		</div>
-	)
+  return (
+    <div className='algorithmsPage'>
+      <ContentContainer tableOfContent={tableOfContent} />
+      <SideMenus
+        onClick={handleTabSelection}
+        selected={selected}
+        tableOfContent={tableOfContent}
+      />
+    </div>
+  )
 }
 
 export default Algorithms
