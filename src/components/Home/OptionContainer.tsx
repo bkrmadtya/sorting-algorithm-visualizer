cf193d2b833a062350096b117d167eb081193215
bkrmadtya <karki.bikram007@gmail.com>
2021-10-03 01:19:29 +0300

    feat: added algorithm option and memoized component

--- src/components/Home/OptionContainer.tsx
+++ src/components/Home/OptionContainer.tsx
@@ -1,16 +1,21 @@
-import React, { ReactEventHandler } from 'react'
+import React, { ReactEventHandler, useCallback, useMemo, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 // components
 import { Header, Dropdown } from '../shared'
 
 // js
-import { changeAnimationSpped, changeArraySize } from '../../store/slice/sorting'
-import { RootState } from 'src/store'
+import { changeAlgorithm, changeAnimationSpped, changeArraySize } from '../../store/slice/sorting'
+import { RootState } from '../../store'
 
 const SizeOption = 'Size'
+const AlgorithmOption = 'Algorithm'
 
-const options = [
+const getMenuOptions = (allAlgorithms: string[]) => [
+  {
+    name: AlgorithmOption,
+    value: allAlgorithms.map(al => ({ key: al, value: al }))
+  },
   {
     name: SizeOption,
     value: [
@@ -23,8 +28,8 @@ const options = [
         value: '25'
       },
       {
-        key: '50',
-        value: '50'
+        key: '35',
+        value: '35'
       }
     ]
   },
@@ -45,38 +50,47 @@ const options = [
       }
     ]
   }
-
 ]
 
 
 const OptionContainer: React.FC = () => {
-  const { arraySize } = useSelector((state: RootState) => state.sorting)
+  const { arraySize, allAlgorithms } = useSelector((state: RootState) => state.sorting)
   const dispatch = useDispatch();
 
-  const handleChangeEvent: ReactEventHandler<HTMLSelectElement> = event => {
+  const handleChangeEvent: ReactEventHandler<HTMLSelectElement> = useCallback(event => {
     const { name, value } = event.currentTarget
     if (name === SizeOption) {
       dispatch(changeArraySize(value))
+    } else if (name === AlgorithmOption) {
+      dispatch(changeAlgorithm(value))
     } else {
       dispatch(changeAnimationSpped(value))
     }
-  }
+  }, [dispatch])
+  const rerender = useRef(0)
 
-  return (
-    <div className='optionContainer'>
-      {options.map(op => (
-        <Header className='panelHeader' key={op.name}>
-          <span>Size: </span>
-          <Dropdown
-            name={op.name}
-            options={op.value}
-            onChange={handleChangeEvent}
-            value={`${arraySize}`}
-          />
-        </Header>))
-      }
-    </div>
-  )
+  const options = useMemo(() => {
+    return getMenuOptions(allAlgorithms)
+  }, [allAlgorithms])
+
+  return useMemo(() => {
+    return (
+      <div className='optionContainer'>
+        {options.map(op => (
+          <Header className='panelHeader' key={op.name}>
+            <span>{op.name}: </span>
+            <Dropdown
+              name={op.name}
+              options={op.value}
+              onChange={handleChangeEvent}
+              value={`${arraySize}`}
+            />
+          </Header>))
+        }
+        {rerender.current++}
+      </div>
+    )
+  }, [arraySize, handleChangeEvent, options])
 }
 
 export default OptionContainer
