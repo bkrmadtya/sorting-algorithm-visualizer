98368a48dd1120ece81488d96fe12a31c7d54a08
bkrmadtya <karki.bikram007@gmail.com>
2021-10-03 00:16:48 +0300

    feat: added currentStep to the state and selectors

--- src/store/slice/sorting.ts
+++ src/store/slice/sorting.ts
@@ -1,13 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 // js
-import { Bar, ISortingAlgorithm, randomArrayGenerator } from '../../utils';
+import { RootState } from '../';
+import { Bar, BarStatus, ISortingAlgorithm, randomArrayGenerator } from '../../utils';
 import { BubbleSort, MergeSort, QuickSort } from '../../algorithms'
 
 interface AlgorithmState {
   selectedAlgorithm: string
   animationSpeed: number
   arraySize: number
+  currentStep: number
   initialArray: Bar[]
   steps: Bar[][]
 }
@@ -23,10 +25,11 @@ const initialAlgorithm = new BubbleSort()
 const initialSteps = initialAlgorithm.sort(initialRandomArray)
 
 const initialState: AlgorithmState = {
-  selectedAlgorithm: BubbleSort.name,
   animationSpeed: 50,
   arraySize: 25,
+  currentStep: 0,
   initialArray: initialRandomArray,
+  selectedAlgorithm: BubbleSort.name,
   steps: initialSteps
 }
 
@@ -37,6 +40,7 @@ export const sorting = createSlice({
     changeArraySize: (state, { payload }) => {
       const randomArray = [...randomArrayGenerator(payload)]
       state.arraySize = payload
+      state.currentStep = 0
       state.initialArray = randomArray
       state.steps = sortArray(Algorithm[state.selectedAlgorithm], randomArray)
     },
@@ -48,19 +52,72 @@ export const sorting = createSlice({
       state.selectedAlgorithm = selectedAlgorithm.name
       state.steps = sortArray(selectedAlgorithm, state.initialArray)
     },
+    goToNextStep: (state) => {
+      if (state.currentStep < state.steps.length - 1) {
+        state.currentStep += 1
+      }
+    },
+    goToPreviousStep: (state) => {
+      if (state.currentStep >= 0) {
+        state.currentStep -= 1
+      }
+    },
     resetSorting: (state) => {
       const randomArray = [...randomArrayGenerator(state.arraySize)]
       state.initialArray = randomArray
+      state.currentStep = 0
       state.steps = sortArray(Algorithm[state.selectedAlgorithm], randomArray)
     }
   }
 })
 
 // Action creators are generated for each case reducer function
-export const { changeArraySize, changeAlgorithm, changeAnimationSpped, resetSorting } = sorting.actions
+export const {
+  changeArraySize,
+  changeAlgorithm,
+  changeAnimationSpped,
+  goToNextStep,
+  goToPreviousStep,
+  resetSorting
+} = sorting.actions
 
 export default sorting.reducer
 
 const sortArray = (algorithm: ISortingAlgorithm, randomArray: Bar[]): Bar[][] => {
   return new (algorithm || Algorithm.BubbleSort)().sort(randomArray)
+}
+
+
+export const getActiveElements = ({ sorting }: RootState): string => sorting.steps[sorting.currentStep]
+  .filter(ele => ele.status === BarStatus.ACTIVE)
+  .map(ele => ele.value).join(", ")
+export const getArraySize = ({ sorting }: RootState): number => sorting.arraySize
+export const getCurrentArray = ({ sorting }: RootState): string => sorting.steps[sorting.currentStep].map(i => i.value).join(", ")
+export const getInitialArray = ({ sorting }: RootState): string => sorting.initialArray.map(i => i.value).join(", ")
+export const getSortingStatus = ({ sorting }: RootState): string => {
+  if (sorting.currentStep === 0) {
+    return BarStatus.UNSORTED
+  } else if (sorting.steps[sorting.currentStep].every(ele => ele.status === BarStatus.SORTED)) {
+    return BarStatus.SORTED
+  } else {
+    return 'SORTING'
+  }
+}
+
+type BottomPanelInfo = {
+  activeElement: string
+  arraySize: number
+  initialArray: string
+  currentArray: string
+  status: string
+}
+
+export const getInfoForBottomPanel = (state: RootState): BottomPanelInfo => {
+  return {
+    arraySize: getArraySize(state),
+    activeElement: getActiveElements(state),
+    initialArray: getInitialArray(state),
+    currentArray: getCurrentArray(state),
+    status: getSortingStatus(state)
+  }
 }
